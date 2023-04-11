8e225a191270663d180b2ebf742925cb09648777
SanketSarmalkar <sanket.sarmalkar2002@gmail.com>
2023-04-11 21:39:53 +0530

    permission solved

--- lib/controller/permissions_handler.dart
+++ lib/controller/permissions_handler.dart
@@ -3,9 +3,13 @@
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:location/location.dart';
+import 'package:app_settings/app_settings.dart';
 
 class PermissionsHandler extends GetxController {
   var allPermissionsGranted = "0".obs;
+  var locationPermission = false.obs;
+  var phonePermission = false.obs;
+  var smsPermission = false.obs;
   Location location = Location();
 
   Future<void> checkService() async {
@@ -14,10 +18,22 @@ class PermissionsHandler extends GetxController {
         await Permission.sms.request().isGranted) {
       allPermissionsGranted.value = "1";
     } else {
-      await Permission.location.request();
-      await Permission.phone.request();
-      await Permission.sms.request();
+      (await Permission.location.request() == true)
+          ? locationPermission.value = true
+          : null;
+      (await Permission.phone.request() == true)
+          ? phonePermission.value = true
+          : null;
+      (await Permission.sms.request() == true)
+          ? smsPermission.value = true
+          : null;
     }
     // print(_permissionGrantedLocation);
+  }
+
+  Future<void> checkServiceAfterDenied() async {
+    if (!await Permission.location.request().isGranted) {
+      AppSettings.openLocationSettings();
+    }
   }
 }
--- lib/screens/request_access.dart
+++ lib/screens/request_access.dart
@@ -16,38 +16,79 @@ class _RequestAccessState extends State<RequestAccess> {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            MaterialButton(
-              shape: const RoundedRectangleBorder(
-                side: BorderSide(color: Colors.black),
-                borderRadius: BorderRadius.all(Radius.circular(75)),
-              ),
-              height: 150,
-              minWidth: 150,
-              color: Colors.blue,
-              onPressed: () async {
-                permissionsHandler.checkService();
-              },
-              child: const Text(
-                "Request Permissions",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                  color: Colors.white,
+        child: Obx(
+          () => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MaterialButton(
+                shape: const RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.black),
+                  borderRadius: BorderRadius.all(Radius.circular(75)),
+                ),
+                height: 150,
+                minWidth: 150,
+                color: Colors.blue,
+                onPressed: () async {
+                  permissionsHandler.checkServiceAfterDenied();
+                },
+                child: const Text(
+                  "Request Permissions",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Text(
-              "Note: Give all necessary permissions",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(" and get all security features"),
-          ],
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Note: Give all necessary permissions",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(" and get all security features"),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Access to Location"),
+                  (permissionsHandler.locationPermission == true)
+                      ? const Icon(
+                          Icons.check_circle,
+                          color: Colors.green,
+                        )
+                      : const Icon(Icons.close_rounded, color: Colors.red)
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Access to Phone"),
+                  (permissionsHandler.phonePermission == true)
+                      ? const Icon(
+                          Icons.check_circle,
+                          color: Colors.green,
+                        )
+                      : const Icon(Icons.close_rounded, color: Colors.red)
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Access to SMS"),
+                  (permissionsHandler.smsPermission == true)
+                      ? const Icon(
+                          Icons.check_circle,
+                          color: Colors.green,
+                        )
+                      : const Icon(Icons.close_rounded, color: Colors.red)
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
